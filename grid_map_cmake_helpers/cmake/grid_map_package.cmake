8b0765771f309e70201ac45b3f279c8c49a9011b
Ryan Friedman <25047695+Ryanf55@users.noreply.github.com>
2025-01-18 14:07:25 -0700

    Revert "Trimmed the branch to only the grid_map_msgs sub directory"
    This reverts commit 478b56d5d15f3b27dc7cb9f8bb1101d66bcb9d8d.

---
+++ grid_map_cmake_helpers/cmake/grid_map_package.cmake
@@ -0,0 +1,40 @@
+# Standard grid_map project setup
+#
+# @public
+#
+macro(grid_map_package)
+  if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
+    message(STATUS "Setting build type to Release as none was specified.")
+    set(CMAKE_BUILD_TYPE "Release" CACHE
+        STRING "Choose the type of build." FORCE)
+    # Set the possible values of build type for cmake-gui
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS
+      "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+  endif()
+
+  if(NOT CMAKE_CXX_STANDARD)
+    set(CMAKE_CXX_STANDARD 17)
+  endif()
+
+  if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    add_compile_options(-Wall -Wextra -Wpedantic -Werror -Wdeprecated -fPIC)
+  endif()
+
+  if(COVERAGE_ENABLED)
+    add_compile_options(--coverage)
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} --coverage")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} --coverage")
+  endif()
+
+  # Defaults for Microsoft C++ compiler
+  if(MSVC)
+    # https://blog.kitware.com/create-dlls-on-windows-without-declspec-using-new-cmake-export-all-feature/
+    set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
+
+    # Enable Math Constants
+    # https://docs.microsoft.com/en-us/cpp/c-runtime-library/math-constants?view=vs-2019
+    add_compile_definitions(
+      _USE_MATH_DEFINES
+    )
+  endif()
+endmacro()
